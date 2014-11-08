0e17bb3003898f76e6bc32f48e90119b4ce16dcd
Alex <amorale4@illinois.edu>
2014-11-07 19:51:24 -0600

    parse

--- src/uiuc/cs446/parser/ParseYelp.java
+++ src/uiuc/cs446/parser/ParseYelp.java
@@ -10,27 +10,88 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 
 public class ParseYelp {
 	public static void main(String[] args){
+		ArrayList<ReviewJ> data = new ArrayList<ReviewJ>();
+		Map<String, MutableInt> freq = new HashMap<String, MutableInt>();
+		Map<String, String> ids = new HashMap<String, String>();
 		try{ 
-		FileReader reader = new FileReader("data/yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_review.json");
-		BufferedReader bf = new BufferedReader( reader);
-	    JsonElement jelement = new JsonParser().parse( bf.readLine());
-	    JsonObject  jobject = jelement.getAsJsonObject();
-	    JsonObject votes = jobject.getAsJsonObject("votes");
-	    String funnyVotes = votes.get("funny").toString();
-	    System.out.println("funny votes: " + funnyVotes);
+			FileReader reader = new FileReader("data/yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_review.json");
+			BufferedReader bf = new BufferedReader( reader);
+			//TODO: need to hash the business values before so we can check the Ids to add to categories.
+			FileReader business = new FileReader("data/yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_business.json");
+			BufferedReader bb = new BufferedReader(business);
+			
+			String line;
+			int ittrs = 11;
+			
+			while( ittrs > 0 && (line = bb.readLine()) != null){
+				JsonElement jelement = new JsonParser().parse(line);
+				JsonObject jobject = jelement.getAsJsonObject();
+				JsonArray categories = jobject.getAsJsonArray("categories");
+			    String business_id = jobject.get("business_id").toString();
+				String name = jobject.get("name").toString(); 
+				if ( categories.contains( new JsonParser().parse("Restaurants")) ){
+			    	System.out.println("This buisiness: " + name + " is a restaurant");
+			    	ids.put(business_id, name);
+			    }
+				//ittrs--;
+			}
+			
+			while (ittrs > 0 && (line = bf.readLine()) != null){
+				JsonElement jelement = new JsonParser().parse(line);
+			    JsonObject  jobject = jelement.getAsJsonObject();
+			    JsonObject votes = jobject.getAsJsonObject("votes");
+			    String funnyVotes = votes.get("funny").toString();
+			    String helpfulVotes = votes.get("useful").toString();
+			    String reviewText = jobject.get("text").toString();
+			    String business_id = jobject.get("business_id").toString();
+			    
+			    if(ids.containsKey(business_id)){
+			    	// Restaurant Review
+			    	ReviewJ temp = new ReviewJ(reviewText, Integer.valueOf(funnyVotes), Integer.valueOf(helpfulVotes),null);
+				    data.add(temp);
+				    // counting distribuion of funny text
+				    MutableInt count = freq.get(funnyVotes);
+				    if (count == null) {
+				        freq.put(funnyVotes, new MutableInt());
+				    }
+				    else {
+				        count.increment();
+				    }   
+			    }	    
+			    ittrs--;
+			}
+			
+			
+			int total_reviews = 0;
+			for (String key : freq.keySet()){
+				System.out.println("there are "+ freq.get(key).value + " funny reviews with number of votes: " + key);
+				total_reviews += freq.get(key).value;
+			}
+			
+			Collections.sort(data);
+			for(ReviewJ r : data){
+				if ( r.funnyVotes > 5){
+					System.out.println(r.funnyVotes);
+					System.out.println(r.getText());
+				}
+			}
 	    
-	    String reviewText = jobject.get("text").toString();
-	    System.out.println("review: " + reviewText);
-	    //JsonArray jarray = jobject.getAsJsonArray("translations");
-	    //jobject = jarray.get(0).getAsJsonObject();
-	    //String result = jobject.get("translatedText").toString();
-	    //return result;
 		} catch (Exception e){
 			e.printStackTrace();
 		}
 	}
+}
+
+class MutableInt {
+	int value = 1; // note that we start at 1 since we're counting
+	public void increment () { ++value;      }
+	public int  get ()       { return value; }
 }
--- src/uiuc/cs446/parser/ReviewJ.java
+++ src/uiuc/cs446/parser/ReviewJ.java
@@ -1,6 +1,60 @@
 package uiuc.cs446.parser;
+import java.util.ArrayList;
+import java.util.Collections;
+
 import com.google.gson.*;
 
-public class ReviewJ {
+public final class ReviewJ implements Comparable<ReviewJ>{
 
+	private String text;
+	private int helpfulVotes;
+	private ArrayList<String> categories;
+
+	public int funnyVotes;
+	
+	/*
+	 * sorts based on largest to smallest.
+	 */
+	public int compareTo(ReviewJ other)
+    {
+		if (this.funnyVotes == other.funnyVotes) return 0;
+        return this.funnyVotes > other.funnyVotes? -1 : 1;
+    }
+	
+	public ReviewJ(String body, int fvotes, int hvotes, ArrayList<String> cat){
+		this.setText(new String(body));
+		this.funnyVotes = fvotes;
+		this.helpfulVotes = hvotes;
+		this.categories = cat;
+	}
+	
+	public static void main(String[] args){
+		ReviewJ temp1 = new ReviewJ("temp1", 0, 0, null);
+		ReviewJ temp2 = new ReviewJ("temp2", 1, 2, null);
+		ReviewJ temp3 = new ReviewJ("temp2", 3, 2, null);
+		
+		ArrayList<ReviewJ> data = new ArrayList<ReviewJ>();
+		data.add(temp1);
+		data.add(temp2);
+
+		data.add(temp3);
+		for(ReviewJ rj : data){
+			System.out.println(rj.funnyVotes);
+		}
+		Collections.sort(data);
+		
+		System.out.println("sorted:");
+		for(ReviewJ rj : data){
+			System.out.println(rj.funnyVotes);
+		}
+		
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
 }
