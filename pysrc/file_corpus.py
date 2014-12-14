c753136b732d03713789e63571088a3115041629
Alex <amorale4@illinois.edu>
2014-12-14 17:06:09 -0600

    cross validation

--- pysrc/file_corpus.py
+++ pysrc/file_corpus.py
@@ -13,5 +13,26 @@ def main():
 			clean_line = line.replace("\\n", "\n")
 			op.write(clean_line)
 
+
+def move_processed():
+	pathToData = "data/temp.output"
+	pathToEnts = "data/proccessed.output"
+	pathToMove = "data/toMoveFiles.output"
+	all_lines = []
+	with open( pathToData, 'r') as f:
+		all_lines = f.readlines()
+	
+	remove_lines = []
+	with open( pathToEnts, 'r') as f:
+		for line in f:
+			fline = line.split('.')
+			remove_lines.append(fline[0] + "." +  fline[1]+'\n')
+
+	remaining = set(all_lines) - set (remove_lines)
+	lines = set(all_lines) - remaining
+
+	with open( pathToMove, 'w') as f:
+		f.writelines(lines)
+
 if __name__ == "__main__":
-	main()
+	move_processed()
